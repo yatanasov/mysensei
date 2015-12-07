9df8f2d3ba2679193927dbd1f67a7bf677504f9e
TalkativeTRex <talkativetrex@gmail.com>
2015-12-07 16:48:32 +0100

    login on new page

--- MySens/Scripts/script.js
+++ MySens/Scripts/script.js
@@ -1,53 +1,10 @@
 (function(){
 	"use strict"
-	var loginBtn = document.querySelector('.loginBtn');
-	var loginOverlay = document.querySelector('.loginOverlay');
-	var signup = document.querySelector('.signup');
-	var signupBg = document.querySelector('.signupBg');
-	var signupBtn = document.querySelector('.signupBtn');
-	var landing = document.querySelector('.landing');
 	var howItWorksBtn = document.querySelector('.howItWorks');
 	var howItWorks = document.querySelector('#howitworks');
 	var btnRotateLeft = document.querySelector('.tBtn1'); 
 	var btnRotateRight = document.querySelector('.tBtn2'); 
 	var rotator = document.querySelector('.buffer');
-
-
-
-	loginBtn.onclick=function(e){
-		e.preventDefault();
-		loginOverlay.classList.toggle('hidden');
-		signupBg.classList.add('hidden');
-
-		var parDir = document.querySelector('.parentRedir');
-
-		parDir.onclick=function(){
-			console.log('hey');
-		}
-
-
-		landing.onclick=function(){
-			loginOverlay.classList.add('hidden');
-		}
-
-		signupBtn.onclick=function(e){
-			e.preventDefault();
-			loginOverlay.classList.add('hidden');
-			signupBg.classList.remove('hidden');
-		}
-
-		signupBg.onclick=function(){
-			signupBg.classList.add('hidden');
-		}
-
-		signup.onclick=function(e){
-			e.stopPropagation();
-		}
-
-		loginOverlay.onclick=function(e){
-			e.stopPropagation();
-		}
-	}
 
 	howItWorksBtn.onclick=function(e){
 		e.preventDefault();
@@ -56,8 +13,6 @@
 			scrollTop: howTop
 		}, 2000);
 	}
-
-	
 
 	function homeMadeCarousel(){
 		var leftPos = 0;
